6b6adb4e1e4be819288c83f1d2c369976c7d3be3
Mahmoud Gohar <goharmahmoud987@gmail.com>
2026-09-21 16:41:29 +0300

    add app structure, assets, and colors

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesForgot Password
+  /// assets/images/Forgot Password.png
+  static const String imagesForgotPassword = "assets/images/Forgot Password.png";
+
+  /// Assets for imagesOnbarding2
+  /// assets/images/onbarding2.png
+  static const String imagesOnbarding2 = "assets/images/onbarding2.png";
+
+  /// Assets for imagesOnbarding3
+  /// assets/images/onbarding3.png
+  static const String imagesOnbarding3 = "assets/images/onbarding3.png";
+
+  /// Assets for imagesOnboarding1
+  /// assets/images/onboarding1.png
+  static const String imagesOnboarding1 = "assets/images/onboarding1.png";
+
+  /// Assets for imagesOrder icon
+  /// assets/images/order icon.png
+  static const String imagesOrderIcon = "assets/images/order icon.png";
+
+  /// Assets for imagesVector1
+  /// assets/images/Vector1.svg
+  static const String imagesVector1 = "assets/images/Vector1.svg";
+
+  /// Assets for imagesVector2
+  /// assets/images/Vector2.svg
+  static const String imagesVector2 = "assets/images/Vector2.svg";
+}
+
